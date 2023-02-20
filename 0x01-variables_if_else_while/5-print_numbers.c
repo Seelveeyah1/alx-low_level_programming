b6e4cb0f994ed335b76adb5cc20637e5db32304a
Omoshie Sylvia <enncee23@gmail.com>
2023-02-20 13:03:35 -0800

    Task five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to print all single digits in base ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int n;
+for (n = 0; n <= 9; n++)
+{
+	printf("%d\n", n);
+}
+return (0);
+}
