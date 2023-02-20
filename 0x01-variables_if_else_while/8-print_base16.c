60ecffefb2cd4d28da6ef2486fa5dcb6a7f322ee
Omoshie Sylvia <enncee23@gmail.com>
2023-02-20 14:45:50 -0800

    Task eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - to print the hexagonal digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int u;
+for (u = 0; u <= 15; u++)
+{
+	putchar(u);
+}
+	putchar('\n');
+return (0);
+}
