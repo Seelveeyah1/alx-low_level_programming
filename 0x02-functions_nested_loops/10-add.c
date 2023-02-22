cc684f4e75d4d55f1ab4d4f8b1cbee3d3b75ad8c
Omoshie Sylvia <enncee23@gmail.com>
2023-02-22 03:27:16 -0800

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - entry point
+ * Description: function that adds two integars and returns the value
+ *
+ * @x: integar one
+ * @y: integar two
+ *
+ * Return:sum.
+ */
+
+int add(int x, int y)
+{
+
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
