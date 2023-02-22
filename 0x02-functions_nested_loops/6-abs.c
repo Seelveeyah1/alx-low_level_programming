9663b099b1890c3470cffdc7c0a915c459a78cd2
Omoshie Sylvia <enncee23@gmail.com>
2023-02-21 18:09:38 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - to print the absolute value of an integar
+ *@int: the integar for which the value is to be printed
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+
+	int r;
+
+	r = _abs(9);
+	_putchar('9');
+return (0);
+}
