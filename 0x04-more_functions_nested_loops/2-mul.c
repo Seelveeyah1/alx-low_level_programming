ee29af09eec0b2505b1f59be75d54ed6f050b1e4
Omoshie Sylvia <enncee23@gmail.com>
2023-02-22 22:20:46 -0800

    Multplication prototype

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * Description: function that multiplies two integars and returns the result
+ *
+ * @a: integar one
+ * @b: integar two
+ *
+ * Return:product
+ */
+
+int mul(int a, int b)
+{
+int product;
+
+product = a * b;
+
+return (product);
+}
