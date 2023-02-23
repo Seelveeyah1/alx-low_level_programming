7775a848238463c8de204c8e78462b8f124640f6
Omoshie Sylvia <enncee23@gmail.com>
2023-02-22 22:40:37 -0800

    Print number prototype

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - entry point
+ *
+ * Description: function that prints numbers 0 to 9
+ * followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
