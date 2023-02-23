ce92bf4f33fa5e7d41ea380d5dc043185585454b
Omoshie Sylvia <enncee23@gmail.com>
2023-02-23 11:32:42 -0800

    Corrections

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_most_numbers - entry point
- * Description: prints numbers from 0 to except 2 and 4
+ * Description: prints numbers from 0 to 9 except 2 and 4
  */
 
 void print_most_numbers(void)
