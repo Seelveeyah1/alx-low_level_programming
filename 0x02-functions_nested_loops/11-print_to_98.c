f2caf07e81d6581a0128d84b7bcd0ae60903d2ad
Omoshie Sylvia <enncee23@gmail.com>
2023-02-22 20:49:36 -0800

    Fibonacci two

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_to_98(int n)
+/**
+ * print_to_98 - entry point
+ * Description: to print all natural numbers from n to 98
+ *
+ * @n: the starting point of the count
+ */
+
+int main()
+{
+int i, n;
+
+/* input upper limit */
+printf("98");
+printf("Natural numbers from n to 98\n", n);
+for (i = n; i <= 98; i++)
+{
+	printf("n");
+}
+}
+
