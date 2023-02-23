34bb9853acdf0cd919c9a0089c5006afa97174e2
Omoshie Sylvia <enncee23@gmail.com>
2023-02-22 23:44:24 -0800

    Ten times the line of numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - entry point
+ * Description: prints 10 times the numbers ranging from
+ * 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int count = 0;
+
+	while (count++ <= 9)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num);
+			_putchar('\n');
+		}
+	}
+}
