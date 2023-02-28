394de727e899ab15ec4d109fcca5b5b7d0706998
Omoshie Sylvia <enncee23@gmail.com>
2023-02-27 16:19:50 -0800

    Pointer aloocation

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ * Description: takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * @n: pointer to be modified
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
