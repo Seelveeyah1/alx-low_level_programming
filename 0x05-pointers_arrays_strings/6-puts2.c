365ca2fa5a4c8d9abd4e4fd03e6a8cce29cb6504
Omoshie Sylvia <enncee23@gmail.com>
2023-02-27 19:47:31 -0800

    Two characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ *
+ * @str: input
+ * Return: string
+ */
+
+void puts2(char *str)
+{
+	int a = 0;
+
+	for (; str[a] != '\0'; a++)
+	{
+	if ((a % 2) == 0)
+		_putchar(str[a]);
+	else
+		continue;
+	}
+	_putchar('\n');
+}
