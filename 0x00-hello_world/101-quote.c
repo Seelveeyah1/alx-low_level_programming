0ec510b74e6426d02d3fef14c139871038f5bdda
Omoshie <enncee23@gmail.com>
2023-07-13 17:15:40 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -5,18 +6,14 @@
  *
  * Description: This program prints a specific message
  * to the standard error.
- * The message is "and that piece of art is useful" -
- * Dora Korpar, 2015-10-19,
- * followed by a new line.
  *
  * Return: Always 1 (indicating an error)
  */
 
 int main(void)
 {
-	char *message = "and that piece of art is
-		useful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
 
-	write(2, message, 52);  /* 2 represents the standard error file descriptor */
+write(2, message, 52);  /* 2 represents the standard error file descriptor */
 	return (1);
 }
