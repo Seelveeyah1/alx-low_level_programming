e392e0d80cde45244f1922eff95579f9edcec328
Omoshie <enncee23@gmail.com>
2023-07-20 07:19:06 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,16 +12,14 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
-
 	int i;
 
 	for (i = 0; i < n; i++)
 		_putchar('_');
 
 	_putchar('\n');
+
+	if (n <= 0)
+		_putchar('\n');
+	return;
 }
