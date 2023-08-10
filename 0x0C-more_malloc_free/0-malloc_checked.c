ff2f710316be46e0e7d458b96c8e1f0eb5e08c8d
Omoshie <enncee23@gmail.com>
2023-08-10 09:50:42 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,9 +4,8 @@
  * malloc_checked - Allocates memory using malloc.
  * @b: The number of bytes to allocate
  *
- * Return: Pointer to the allocated memory
- * If malloc fails, the program exits with
- * a status value of 98
+ * Return: Pointer to the allocated memory. If malloc
+ * fails, the program exits with a status value of 98
  */
 
 void *malloc_checked(unsigned int b)
@@ -15,7 +14,6 @@ void *malloc_checked(unsigned int b)
 
 	if (allocated_memory == NULL)
 	{
-		_putchar('!');
 		exit(98);
 	}
 
