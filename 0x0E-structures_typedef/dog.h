8adedab23974d54bbf62901d93f9936ad1ec9ac2
Omoshie <enncee23@gmail.com>
2023-08-14 14:17:47 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+/**
+ * struct dog - Structure representing information about
+ * a dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the name of the dog's owner
+ *
+ * Description: This structure defines the attributes of a
+ * dog including its name, age, and owner's name.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
