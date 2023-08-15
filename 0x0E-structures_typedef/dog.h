10689bbf73ea4f5b5fade5f54fa4aba85a6d43d8
Omoshie <enncee23@gmail.com>
2023-08-15 14:08:45 -0700

    Task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,11 +26,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
 #endif
