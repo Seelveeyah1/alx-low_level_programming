bb014c56dbfb6bad63c20048f9d71836c0bcb0be
Omoshie <enncee23@gmail.com>
2023-08-24 14:14:22 -0700

    Task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+    hello db "Hello, Holberton,",0
+    format db "%s", 10, 0  ; "%s" followed by newline character
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rdi, format
+    mov rsi, hello
+    call printf
+    pop rbp
+    ret
